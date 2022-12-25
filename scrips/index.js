970253d45787ec7a79ea0f41ca3723a2490c1b4c
Roman Tkachuk <tkachukrr@gmail.com>
2022-12-26 00:28:40 +0100

    Finished myIterable.js

--- scrips/index.js
+++ scrips/index.js
@@ -1,4 +1,6 @@
 import { inputValTest } from "./testInputVal.js";
+import { iterable } from "./myIterable.js";
+// import { makeItareable } from "./myIterable.js";
 
 // let userFirstVal = prompt("Введите число:", "");
 // let userSecondVal = prompt("Введите число:", "");
---
+++ scrips/myIterable.js
@@ -0,0 +1,27 @@
+Object.prototype[Symbol.iterator] = function () {
+  if (!this.from || !this.to) {
+    throw new Error("проверте наличие свойств from, to у объекта");
+  }
+
+  if (this.from > this.to) {
+    throw new Error(
+      "проверте правильность значений свойств from, to у объекта"
+    );
+  }
+
+  const endIterationVal = this.to;
+  let startIterationVal = this.from - 1;
+
+  return {
+    next() {
+      if (startIterationVal < endIterationVal) {
+        startIterationVal++;
+        return { done: false, value: startIterationVal };
+      } else {
+        return { done: true };
+      }
+    },
+  };
+};
+
+export const iterable = {};
