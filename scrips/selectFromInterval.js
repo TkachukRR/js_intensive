ecc6e429edb49605c56f96231a5b3c00469f5a51
Roman Tkachuk <tkachukrr@gmail.com>
2022-12-25 17:13:05 +0100

    Finished function  selectFromInterval

---
+++ scrips/selectFromInterval.js
@@ -0,0 +1,26 @@
+export function selectFromInterval(
+  innerArray,
+  firstIntervalVal,
+  secondIntervalVal
+) {
+  if (!Array.isArray(innerArray)) {
+    throw new Error("первый аргумент фунции - массив чисел");
+  }
+
+  if (innerArray.some((element) => typeof element !== "number")) {
+    throw new Error("масив должен состоять из чисел");
+  }
+
+  if (
+    typeof firstIntervalVal !== "number" ||
+    typeof secondIntervalVal !== "number"
+  ) {
+    throw new Error("значения интервала - числа");
+  }
+
+  return innerArray.filter(
+    (element) =>
+      (element >= firstIntervalVal && element <= secondIntervalVal) ||
+      (element <= firstIntervalVal && element >= secondIntervalVal)
+  );
+}
