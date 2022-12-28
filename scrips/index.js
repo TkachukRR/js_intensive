c9236da083dd51f69b6d8b03c545fdfc15d5caba
Roman Tkachuk <tkachukrr@gmail.com>
2022-12-28 19:57:05 +0100

    Finished myFilter.js

--- scrips/index.js
+++ scrips/index.js
@@ -1,7 +1,6 @@
 import { inputValTest } from "./testInputVal.js";
 import { addArrayPrototypeProperty } from "./addArrayPrototypeProperty.js";
 import { myFilter } from "./myFilter.js";
-
 // let userFirstVal = prompt("Введите число:", "");
 // let userSecondVal = prompt("Введите число:", "");
 
