2fa1a82b875b005c1eabe993d33e932ec2aaf29f
Roman Tkachuk <tkachukrr@gmail.com>
2022-12-29 12:43:42 +0100

    Added import myStack to index.js

--- scrips/index.js
+++ scrips/index.js
@@ -6,32 +6,3 @@ import { myStack } from "./stack.js";
 
 // let testedFisrtVal = inputValTest(userFirstVal);
 // let testedSecondVal = inputValTest(userSecondVal);
-
-const stackTasks = new myStack();
-// const stackTasks = new Stack({ data: [] });
-
-console.log(stackTasks.length());
-
-stackTasks.push("task1");
-console.log(stackTasks.data.toString());
-console.log("peek:", stackTasks.peek());
-
-stackTasks.push("task2");
-console.log(stackTasks.data.toString());
-console.log("peek:", stackTasks.peek());
-
-stackTasks.push("task3");
-console.log(stackTasks.data.toString());
-console.log("peek:", stackTasks.peek());
-
-console.log(stackTasks.length());
-console.log(stackTasks.data);
-
-stackTasks.pop();
-stackTasks.pop();
-stackTasks.pop();
-stackTasks.pop();
-
-console.log(stackTasks.data);
-
-console.log(stackTasks.peek());
