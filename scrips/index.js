f3a39f423526cb300da7a96872a1677114a7c6b4
Roman Tkachuk <tkachukrr@gmail.com>
2022-12-29 12:40:46 +0100

    Finished stack.js

--- scrips/index.js
+++ scrips/index.js
@@ -1,7 +1,37 @@
 import { inputValTest } from "./testInputVal.js";
+import { myStack } from "./stack.js";
 
-let userFirstVal = prompt("Введите число:", "");
-let userSecondVal = prompt("Введите число:", "");
+// let userFirstVal = prompt("Введите число:", "");
+// let userSecondVal = prompt("Введите число:", "");
 
-let testedFisrtVal = inputValTest(userFirstVal);
-let testedSecondVal = inputValTest(userSecondVal);
+// let testedFisrtVal = inputValTest(userFirstVal);
+// let testedSecondVal = inputValTest(userSecondVal);
+
+const stackTasks = new myStack();
+// const stackTasks = new Stack({ data: [] });
+
+console.log(stackTasks.length());
+
+stackTasks.push("task1");
+console.log(stackTasks.data.toString());
+console.log("peek:", stackTasks.peek());
+
+stackTasks.push("task2");
+console.log(stackTasks.data.toString());
+console.log("peek:", stackTasks.peek());
+
+stackTasks.push("task3");
+console.log(stackTasks.data.toString());
+console.log("peek:", stackTasks.peek());
+
+console.log(stackTasks.length());
+console.log(stackTasks.data);
+
+stackTasks.pop();
+stackTasks.pop();
+stackTasks.pop();
+stackTasks.pop();
+
+console.log(stackTasks.data);
+
+console.log(stackTasks.peek());
