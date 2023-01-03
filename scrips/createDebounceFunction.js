8c1a688c9e8a5a2f4783ed06d59f2f33319662fb
Roman Tkachuk <tkachukrr@gmail.com>
2023-01-03 13:34:38 +0100

    Fixed and deleted consloe.logs in createDebounceFunction

--- scrips/createDebounceFunction.js
+++ scrips/createDebounceFunction.js
@@ -1,17 +1,13 @@
 export function createDebounceFunction(func, delay) {
   let timeout;
-  let fistTimePoint = Date.now();
-  console.log(fistTimePoint);
 
   return function () {
-    clearTimeout(timeout);
+    if (timeout) {
+      clearTimeout(timeout);
+    }
 
     timeout = setTimeout(() => {
-      let secondTimePoint = Date.now();
-      console.log(secondTimePoint);
-      console.log("delta: ", secondTimePoint - fistTimePoint);
-
-      func.apply();
+      func();
     }, delay);
   };
 }
