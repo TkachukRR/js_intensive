180790695088b9d0e0686219888ee268d0b6b1ce
Roman Tkachuk <tkachukrr@gmail.com>
2023-01-03 14:07:20 +0100

    Fixed class Calculator

--- scrips/index.js
+++ scrips/index.js
@@ -6,21 +6,3 @@ import { Calculator } from "./Calculator.js";
 
 // let testedFisrtVal = inputValTest(userFirstVal);
 // let testedSecondVal = inputValTest(userSecondVal);
-
-const myCalc = new Calculator(
-  1.111111222222222222222222223333333333333333333333333,
-  233333333333333333333333333333333333333333
-);
-myCalc.getX();
-myCalc.getY();
-
-// myCalc.setX(1);
-// myCalc.setY(2);
-
-myCalc.getX();
-myCalc.getY();
-
-myCalc.logSum();
-myCalc.logMul();
-myCalc.logSub();
-myCalc.logDiv();
